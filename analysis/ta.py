a9b9403f67aa9ae51d6650fe04ada35488ab3db9
nqhung291 <nqhung291@gmail.com>
2020-06-18 21:16:38 +0700

    add watching stock

--- analysis/ta.py
+++ analysis/ta.py
@@ -8,6 +8,7 @@ today = -1
 
 
 def bounce_strategy(end_date):
+    watching_stock_list = []
     enter_stock_list = []
     for symbol in db.get_stock_symbol():
         (open, high, low, close, date) = db.get_price(symbol, end_date)
@@ -37,67 +38,49 @@ def bounce_strategy(end_date):
         }
 
         candle_pattern = False
-        enter_trade = False
+        indicator = False
 
-        if single_candle_reversal(reversal_candle, confirmation_candle, ema_18):
+        if single_candle_reversal(reversal_candle, ema_18):
             candle_pattern = True
-            # print('Single candle bounce 18 + confirmation', symbol, date[today])
-        if single_candle_reversal(reversal_candle, confirmation_candle, ema_50):
+        if single_candle_reversal(reversal_candle, ema_50):
             candle_pattern = True
-            # print('Single candle bounce 50 + confirmation', symbol, date[today])
-        if doji_candle_reversal(doji_list[today], reversal_candle, confirmation_candle, ema_18):
+        if doji_candle_reversal(doji_list[today], reversal_candle, ema_18):
             candle_pattern = True
-            # print('Doji candle bounce 18 + confirmation', symbol, date[today])
-        if doji_candle_reversal(doji_list[today], reversal_candle, confirmation_candle, ema_50):
+        if doji_candle_reversal(doji_list[today], reversal_candle, ema_50):
             candle_pattern = True
-            # print('Doji candle bounce 50 + confirmation', symbol, date[today])
         else:
-            if basic_two_candle_reversal(
-                    prev_reversal['low'], reversal_candle, confirmation_candle, ema_18
-            ):
+            if basic_two_candle_reversal(prev_reversal['low'], reversal_candle, ema_18):
                 candle_pattern = True
-                # print('2 Candle reversal + Confirmation 18', symbol, date[today])
-            if basic_two_candle_reversal(
-                    prev_reversal['low'], reversal_candle, confirmation_candle, ema_50
-            ):
+            if basic_two_candle_reversal(prev_reversal['low'], reversal_candle, ema_50):
                 candle_pattern = True
-                # print('2 Candle reversal + Confirmation 50', symbol, date[today])
             else:
-                if inside_bar_two_candle_reversal(
-                        prev_reversal, reversal_candle, confirmation_candle, ema_18
-                ):
+                if inside_bar_two_candle_reversal(prev_reversal, reversal_candle, ema_18):
                     candle_pattern = True
-                    # print('2 Candle reversal inside bar + Confirmation 18', symbol, date[today])
-                if inside_bar_two_candle_reversal(
-                        prev_reversal, reversal_candle, confirmation_candle, ema_50
-                ):
+                if inside_bar_two_candle_reversal(prev_reversal, reversal_candle, ema_50):
                     candle_pattern = True
-                    # print('2 Candle reversal inside bar + Confirmation 50', symbol, date[today])
-                if two_candle_reversal_trade_through(
-                        prev_reversal, reversal_candle, confirmation_candle, ema_18
-                ):
+                if two_candle_reversal_trade_through(prev_reversal, reversal_candle, ema_18):
                     candle_pattern = True
-                if two_candle_reversal_trade_through(
-                        prev_reversal, reversal_candle, confirmation_candle, ema_50
-                ):
+                if two_candle_reversal_trade_through(prev_reversal, reversal_candle, ema_50):
                     candle_pattern = True
 
         if candle_pattern is True:
             if slowk[today] < 30:
                 if macd[today] > macdsignal[today]:  # if macd is bullish
-                    enter_trade = True
-                else:
+                    indicator = True
+                else: # macd is bearish and not crossed bullish less than 5 days
                     macd_period = macd[today - 4:]
                     macd_signal_period = macdsignal[today - 4:]
                     if not np.isnan(macd_period).any() \
                             and not np.isnan(macd_signal_period).any() \
                             and np.all(macd_period < macd_signal_period):
-                        enter_trade = True
-                if enter_trade:
-                    sl = np.round(confirmation_candle['low'] - 0.05, 2)
-                    entry = np.round(confirmation_candle['high'] + 0.05, 2)
-                    tp = np.round(entry + (entry - sl) * 2, 2)
-                    signal = f'Enter {symbol} {date[today]} | sl: {sl} | entry: {entry} | tp: {tp}'
-                    enter_stock_list.append(signal)
-    return enter_stock_list
+                        indicator = True
+        if indicator:
+            watching_stock_list.append(symbol)
+            if is_confirmation_candle(reversal_candle, confirmation_candle):
+                sl = np.round(confirmation_candle['low'] - 0.05, 2)
+                entry = np.round(confirmation_candle['high'] + 0.05, 2)
+                tp = np.round(entry + (entry - sl) * 2, 2)
+                signal = f'{symbol} | sl: {sl} | entry: {entry} | tp: {tp}'
+                enter_stock_list.append(signal)
+    return watching_stock_list, enter_stock_list
 
